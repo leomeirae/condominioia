74cc1ab1bd2457aa35405f123022ddb3ad888c15
seu-usuario <seu-email@exemplo.com>
2025-02-14 17:19:12 -0300

    chore: add vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000']
+    }
+  },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
   }
 };
 
